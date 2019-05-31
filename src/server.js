ef44197bafbb36ce8ee88afb284ee09921553a89
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-05-30 22:10:09 -0500

    set up for client testing

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const app = require('./app');
-const { PORT, DB_URL } = require('./config');
+const { PORT, TEST_DB_URL } = require('./config');
 const knex = require('knex');
 
 const knexInstance = knex({
   client: 'pg',
-  connection: DB_URL
+  connection: TEST_DB_URL
 });
-
+//WARNING CONNECTED TO TEST DB
 
 app.set('db', knexInstance);
-app.listen(PORT, console.log('Welcome to the MaTriX', PORT));
+app.listen(PORT, console.log('Welcome to the MaTriX-TESTx', PORT));
 
 
 
